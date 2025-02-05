32fabab9aaa567e0dcf4ef8ed2f0f302e73e20ca
JasmineButterfield0 <ja895969@ucf.edu>
2025-02-04 23:15:46 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ root.render(
   </React.StrictMode>
 );
 
+// 1.Render Menu Items:
+const Sidebar = ({ initialMenuItems }) => {
+  return (
+    <div className="sidebar">
+      <ul>
+        {initialMenuItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
