64bbe3c49283ce4c58516831c4a8b7e28d6df91a
JasmineButterfield0 <ja895969@ucf.edu>
2025-02-04 23:29:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,41 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-// 1.Render Menu Items:
-const Sidebar = ({ initialMenuItems }) => {
-  return (
-    <div className="sidebar">
-      <ul>
-        {initialMenuItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Sidebar;
-
-//2. Maintain Menu State
-// Sidebar.js
-import React, { useState } from 'react';
-
-const Sidebar = ({ initialMenuItems }) => {
-  // TODO: Maintain the state for menu items using useState
-  const [menuItems, setMenuItems] = useState(initialMenuItems);
-
-  return (
-    <div className="sidebar">
-      <ul>
-        {menuItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Sidebar;
 
 
 
