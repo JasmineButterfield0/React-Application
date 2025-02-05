d5d299cd17ad491d38cd52e8af949fd995bf7de0
JasmineButterfield0 <ja895969@ucf.edu>
2025-02-04 23:22:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,6 +26,30 @@ const Sidebar = ({ initialMenuItems }) => {
 
 export default Sidebar;
 
+//2. Maintain Menu State
+// Sidebar.js
+import React, { useState } from 'react';
+
+const Sidebar = ({ initialMenuItems }) => {
+  // TODO: Maintain the state for menu items using useState
+  const [menuItems, setMenuItems] = useState(initialMenuItems);
+
+  return (
+    <div className="sidebar">
+      <ul>
+        {menuItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
